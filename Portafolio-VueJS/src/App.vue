b197609f837bc56cd0d31e69a1f90fce400ac4b7
Emmanuel Herrera <emmanuelhk34@gmail.com>
2024-10-07 08:06:06 -0300

    Incorporo Portafolio-Web del 1er semestre y Portafolo-VueJS (Capitulo 1 y 2)

---
+++ Portafolio-VueJS/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+/* AQUÍ VA TODO PURO JS. Con setup luego en template utilizando doble-llaves se ejecutara directamente el codigo js*/
+const nombreApellido = "Emmanuel Herrera Gandini";
+
+</script>
+
+<template>
+  <!--AQUÍ VA HTML-->
+  <header>
+    <h1> {{ nombreApellido }} </h1>
+    <p> Bienvenido a Vue 3 </p>
+  </header>
+</template>
+
+<style scoped>
+/*
+AQUÍ VA EL STYLE CSS
+*/ 
+
+</style>
